d484410ec5431e4eeac54c278cf2b35b40bd8051
ariel <arieldiaz05@gmail.com>
2023-08-10 20:14:38 +0200

    Fix error

--- src/main/java/com/example/demo/entities/UsuariosBuilder.java
+++ src/main/java/com/example/demo/entities/UsuariosBuilder.java
@@ -7,30 +7,30 @@ public class UsuariosBuilder {
     private UsuariosBuilder() { }
 
     public UsuariosBuilder(String nombreUsuario){
-        usuario.nombre = "";
-        usuario.apellidos = "";
-        usuario.email = "";
-        usuario.nivelAcceso = 0;
+//        usuario.nombre = "";
+//        usuario.apellidos = "";
+//        usuario.email = "";
+//        usuario.nivelAcceso = 0;
         usuario.nombreUsuario = nombreUsuario;
     }
 
     public UsuariosBuilder conNombre(String nombre){
-        usuario.nombre = nombre.length() > 0 ? usuario.nombre : "sin nombre";
+        usuario.nombre = nombre.length() > 0 ? nombre : "sin nombre";
         return this;
     }
 
     public UsuariosBuilder conApellido(String apellidos){
-        usuario.apellidos = apellidos.length() > 0 ? usuario.apellidos : "sin apellidos";
+        usuario.apellidos = apellidos.length() > 0 ? apellidos : "sin apellidos";
         return this;
     }
 
     public UsuariosBuilder conEmail(String email){
-        usuario.email = email.length() > 0 ? usuario.email : "sin email";
+        usuario.email = email.length() > 0 ? email : "sin email";
         return this;
     }
 
     public UsuariosBuilder conNivelAcceso(int nivelAcceso){
-        usuario.nivelAcceso = nivelAcceso > 0 ? usuario.nivelAcceso : 0;
+        usuario.nivelAcceso = nivelAcceso > 0 ? nivelAcceso : 0;
         return this;
     }
 
