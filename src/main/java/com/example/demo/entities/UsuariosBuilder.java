ca31b81c6ea7b818c65f6293f2828687bad62876
ariel <arieldiaz05@gmail.com>
2023-08-11 12:00:26 +0200

    Update documentatio

--- src/main/java/com/example/demo/entities/UsuariosBuilder.java
+++ src/main/java/com/example/demo/entities/UsuariosBuilder.java
@@ -1,12 +1,29 @@
 package com.example.demo.entities;
 
+import org.springframework.lang.NonNull;
+
+/**
+ * La clase usuariosBuilder implementa  implementa el patron "builder" para la creacion de un objeto Usuario con ciertas
+ * caracteristicas. En el caso de no completar los campos de nombre, apellidos, email autocompleta con un valor por
+ * defecto "sin nombre", "sin apellido"...
+ */
+
+//INFORMATIVO: Ariel15682@11/08/23 - Se incorpora el uso de anotacion "@NotNull"
+
+/* INTENCIONALIDAD: Con el uso de la anotacion "NotNull" provista por Spring se pretende hacer obligatorio completar
+* el campo nombreUsuario (username) */
+
+//ATRIBUCION: Codigo Original: https://github.com/Ariel15682/demo-Usuario por @Ariel15682
+
+// MENCIONES: Si quedan dudas hablar con @Ariel15682
+
 public class UsuariosBuilder {
 
     private final Usuario usuario = new Usuario();
 
     private UsuariosBuilder() { }
 
-    public UsuariosBuilder(String nombreUsuario){
+    public UsuariosBuilder(@NonNull String nombreUsuario){
 //        usuario.nombre = "";
 //        usuario.apellidos = "";
 //        usuario.email = "";
